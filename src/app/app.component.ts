80e9ecd9b8e515451cea4b57152b5aceee24b381
razorrahul123 <rahul.kr.sushant@gmail.com>
2017-01-13 13:28:42 +0530

    Frontend UI with Foundation instead of Bootstrap UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit} from '@angular/core';
+import {FirebaseService} from './services/firebase.service';
+import {Business} from "./Business";
+import {Category} from "./Category";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FirebaseService]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements  OnInit{
+  businesses:Business[];
+  categories: Category[];
+  appState: string;
+  activekey: string;
+
+
+  constructor(private _firebaseService:FirebaseService) {
+
+  }
+
+  ngOnInit(){
+    this._firebaseService.getBusinesses().subscribe(businesses => {
+      this.businesses = businesses;
+    });
+    this._firebaseService.getCategories().subscribe(categories => {
+      this.categories=categories;
+    });
+  }
+
+  changeState(state,key){
+    console.log('Changing state to: '+state);
+    if(key){
+      console.log('Changing key to: '+key);
+      this.activekey= key;
+    }
+    this.appState= state;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { MaterialModule } from '@angular/material';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCloX7T9RlwDMRq_9_0R9SEm5RZ0vSpLug',
+  authDomain: 'businesscontacts-aaa60.firebaseapp.com',
+  databaseURL: 'https://businesscontacts-aaa60.firebaseio.com',
+  storageBucket: 'businesscontacts-aaa60.appspot.com',
+  messagingSenderId: '316378907212'
+};
+
 
 @NgModule({
   declarations: [
@@ -12,7 +24,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
