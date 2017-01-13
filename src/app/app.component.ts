ad69c2fca9c0299dd435e88fc1dfcaefd2ee3854
razorrahul123 <rahul.kr.sushant@gmail.com>
2017-01-13 17:25:17 +0530

    Added Edit and Delete Component for the proper functioning of the app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,16 @@ export class AppComponent implements  OnInit{
   categories: Category[];
   appState: string;
   activekey: string;
+  activeCompany: string;
+  activeCategory: string;
+  activeYearsInBusiness: string;
+  activeDescription: string;
+  activePhone:string;
+  activeEmail: string;
+  activeStreetAddress: string;
+  activeCity: string;
+  activeState: string;
+  activeZipcode: string;
 
 
   constructor(private _firebaseService:FirebaseService) {
@@ -36,4 +46,78 @@ export class AppComponent implements  OnInit{
     }
     this.appState= state;
   }
+  filterCategory(category){
+    this._firebaseService.getBusinesses(category).subscribe(businesses => {
+      this.businesses = businesses;
+    });
+      }
+
+   addBusiness(
+     company: string,
+     category: string,
+     years_in_business: number,
+     description: string,
+     phone:string,
+     email: string,
+     street_address: string,
+     city: string,
+     state: string,
+     zipcode: string
+   ){
+     var created_at = new Date().toString();
+
+     var newBusiness = {
+       company: company,
+       category: category,
+       years_in_business: years_in_business,
+       description: description,
+       phone:phone,
+       email: email,
+       street_address: street_address,
+       city: city,
+       state: state,
+       zipcode: zipcode,
+       created_at: created_at
+     }
+      //console.log(newBusiness);
+     this._firebaseService.addBusiness(newBusiness);
+     this.changeState('default',this);
+   }
+
+    showEdit(business){
+       this.changeState('edit',business.$key);
+       this.activeCompany= business.company;
+       this.activeCategory= business.category;
+       this.activeYearsInBusiness = business.years_in_business;
+       this.activeDescription = business.description;
+       this.activePhone = business.phone;
+       this.activeEmail = business.email;
+       this.activeStreetAddress = business.street_address;
+       this.activeCity = business.city;
+       this.activeState = business.state;
+       this.activeZipcode = business.zipcode;
+    }
+
+    updateBusiness(){
+      var updBusiness= {
+        company:this.activeCompany,
+        category:this.activeCategory,
+        years_in_business:this.activeYearsInBusiness,
+        description:this.activeDescription,
+        phone:this.activePhone,
+        email:this.activeEmail,
+        street_address:this.activeStreetAddress,
+        city:this.activeCity,
+        state:this.activeState,
+        zipcode: this.activeZipcode
+    }
+    this._firebaseService.updateBusiness(this.activekey, updBusiness)
+      this.changeState('default',this);
+    }
+
+    deleteBusiness(key){
+      this._firebaseService.deleteBusiness(key);
+      this.changeState('default',this);
+    }
+
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -13,16 +13,35 @@ export class FirebaseService{
 
   }
 
-  getBusinesses() {
-    this.businesses = this._af.database.list('/businesses') as
-      FirebaseListObservable<Business[]>
+  getBusinesses(category: string= null) {
+    if (category != null){
+      this.businesses = this._af.database.list('/businesses',{query: {
+        orderByChild: 'category',
+        equalTo: category
+      }}) as
+        FirebaseListObservable<Business[]>
+    } else{
+      this.businesses = this._af.database.list('/businesses') as
+        FirebaseListObservable<Business[]>
+    }
+
     return this.businesses;
   }
     getCategories(){
       this.categories = this._af.database.list('/categories') as
         FirebaseListObservable<Category[]>
       return this.categories;
+  }
 
+  addBusiness(newBusiness){
+      return this.businesses.push(newBusiness);
+  }
+
+  updateBusiness(key, updBusiness){
+    return this.businesses.update(key, updBusiness);
+  }
+  deleteBusiness(key){
+    return this.businesses.remove(key);
   }
 }
 
